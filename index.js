7c488049a66087874f60fb0c192306e30c2eadf8
Deepak <99683019+Deepaksahnii@users.noreply.github.com>
2022-12-19 00:26:44 +0530

    Added some JavaScript functionality

--- index.js
+++ index.js
@@ -1,5 +1,66 @@
-
-// let user = prompt("S, W, or G");
+let cpuI = Math.floor(Math.random() * 3);
+let cpu = ["S", "W", "G"][cpuI];
 let btn1 = document.getElementById('btn1');
+
+
+// ****EventListener for btn 1******
+
+btn1.addEventListener('click', function (e) {
+    e.preventDefault();
+    let value = "S";
+    function output() {
+        if (cpu === "S" && value) {
+            return "Nobody wins";
+        }
+        else if (cpu === "W" && value) {
+            return "You win.";
+        }
+        else if (cpu === "G" && value) {
+            return "Computer wins.";
+        } 
+    }
+    // ****Displaying the result******
+    let result = document.getElementById('result').innerHTML += output();
+})
+
+// ****EventListener for btn 2******
+
 let btn2 = document.getElementById('btn2');
+btn2.addEventListener('click', function (e) {
+    e.preventDefault();
+    let value = "W";
+    function output() {
+        if (cpu === "W" && value) {
+            return "Nobody wins";
+        }
+        else if (cpu === "S" && value) {
+            return "Computer wins.";
+        }
+        else if (cpu === "G" && value) {
+            return "You win.";
+        } 
+    }
+    // ****Displaying the result******
+    let result = document.getElementById('result').innerHTML= output();
+})
+
+// ****EventListener for btn 3******
+
 let btn3 = document.getElementById('btn3');
+btn3.addEventListener('click', function (e) {
+    e.preventDefault();
+    let value = "G";
+    function output() {
+        if (cpu === "G" && value) {
+            return "Nobody wins";
+        }
+        else if (cpu === "S" && value) {
+            return "You win.";
+        }
+        else if (cpu === "W" && value) {
+            return "Computer wins.";
+        } 
+    }
+    // ****Displaying the result******
+    let result = document.getElementById('result').innerHTML= output();
+})
